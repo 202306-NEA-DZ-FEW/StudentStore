030a87cedd948c521b0bf24838fbbc5ec33096c3
Katia Ghezali <ghezalikatia@hotmail.com>
2023-10-14 01:59:24 +0100

    fix(prettier): format the code manually
    Tailwind-CSS-And-DaisyUI-Setup #2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('daisyui'),
-
-  ],
-}
-
+    content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+};
