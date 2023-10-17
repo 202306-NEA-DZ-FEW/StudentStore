4080bd1b015e42958348faeb2be4893aab18475e
Katia Ghezali <ghezalikatia@hotmail.com>
2023-10-17 22:01:03 +0100

    feat(search bar): add the search functionnality to the search bar
    SearchBar #13

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { ImSearch } from "react-icons/im";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "@/util/firebase";
 
-function SearchBar() {
-    return <div>SearchBar</div>;
-}
+const SearchBar = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const handleSearch = async () => {
+        setLoading(true);
+        let q;
+        if (searchTerm.trim() === "") {
+            // If the search term is empty, do not perform a search
+            setProducts([]);
+            setLoading(false);
+            return;
+        }
+        q = query(collection(db, "products"), where("name", ">=", searchTerm));
+        try {
+            const querySnapshot = await getDocs(q);
+            const productsArray = [];
+            querySnapshot.forEach((doc) => {
+                productsArray.push({ id: doc.id, ...doc.data() });
+            });
+
+            setProducts(productsArray);
+            setLoading(false);
+        } catch (error) {
+            console.error("Error getting products: ", error);
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        // Call the search function when searchTerm changes
+        handleSearch();
+    }, [searchTerm]);
+
+    return (
+        <div className='relative mx-auto max-w-md'>
+            <input
+                type='text'
+                placeholder='Search here...'
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                className='w-full border border-gray-300 rounded-full py-2 px-4 pl-8 focus:outline-none'
+            />
+            <div className='absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500'>
+                <ImSearch />
+            </div>
+
+            {loading && <p>Loading...</p>}
+            {!loading && products.length > 0 && (
+                <ul>
+                    {products.map((product) => (
+                        <li key={product.id}>
+                            {product.name} - ${product.price} -{" "}
+                            {product.category}
+                        </li>
+                    ))}
+                </ul>
+            )}
+            {!loading && products.length === 0 && <p>No products found.</p>}
+        </div>
+    );
+};
 
 export default SearchBar;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,11 @@ import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import * as React from "react";
 import { db } from "@/util/firebase";
+import { useCollection } from "react-firebase-hooks/firestore";
 import { collection, getDocs } from "firebase/firestore";
 import Layout from "@/layout/Layout";
+import SearchBar from "@/components/SearchBar/SearchBar";
+import { Firestore } from "firebase/firestore";
 
 export default function HomePage() {
     const { t } = useTranslation("common");
@@ -16,17 +19,26 @@ export default function HomePage() {
         );
     });
     return (
-        <Layout>
-            <p>{t("test")}</p>
-            <div style={{ display: "flex", flexDirection: "row", gap: "20px" }}>
-                <Link href='/' locale='en'>
-                    English
-                </Link>
-                <Link href='/' locale='ar'>
-                    العربية
-                </Link>
-            </div>
-        </Layout>
+        <>
+            <Layout>
+                <p>{t("test")}</p>
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        gap: "20px",
+                    }}
+                >
+                    <Link href='/' locale='en'>
+                        English
+                    </Link>
+                    <Link href='/' locale='ar'>
+                        العربية
+                    </Link>
+                </div>
+            </Layout>
+            <SearchBar />
+        </>
     );
 }
 
