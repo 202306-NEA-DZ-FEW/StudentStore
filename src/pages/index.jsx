549b9a7365b874afb7c99cdd6163454a3a399ec9
benarba mohammed <tewfikhotspur@gmail.com>
2023-10-23 19:20:47 +0200

    feat(language component): language Translation dropdown
    fix #15

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,12 +3,9 @@ import Link from "next/link";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import * as React from "react";
-
-import ProductCard from "@/components/ProductCard/ProductCard";
-
 import Layout from "@/layout/Layout";
 import { db } from "@/util/firebase";
-import SearchBar from "@/components/SearchBar/SearchBar";
+import Language from "@/components/Language/Language";
 
 export default function HomePage() {
     const { t } = useTranslation("common");
@@ -21,8 +18,6 @@ export default function HomePage() {
     });
     return (
         <Layout>
-            <ProductCard />
-            <SearchBar />
             <p>{t("test")}</p>
             <div style={{ display: "flex", flexDirection: "row", gap: "20px" }}>
                 <Link href='/' locale='en'>
@@ -32,6 +27,7 @@ export default function HomePage() {
                     العربية
                 </Link>
             </div>
+            <Language />
         </Layout>
     );
 }
