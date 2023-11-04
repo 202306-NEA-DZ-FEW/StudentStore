8808d1f030fd8aaf5bf6c4c2a2db0e93bd9daa37
benarba mohammed <tewfikhotspur@gmail.com>
2023-11-04 20:45:13 +0100

    feat(signin): translate signin page into arabic and french

--- src/pages/signin/index.jsx
+++ src/pages/signin/index.jsx
@@ -4,12 +4,15 @@ import GoogleButton from "@/components/GoogleButton/GoogleButton";
 import TwitterButton from "@/components/TwitterButton/TwitterButton";
 import { useAuth } from "@/context/AuthContext";
 import Layout from "@/layout/Layout";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 export default function SignIn() {
+    const { t } = useTranslation("sign");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const { login } = useAuth();
@@ -23,7 +26,7 @@ export default function SignIn() {
             await login(email, password);
             route.push("/");
         } catch (error) {
-            toast.error("Failed to log in");
+            toast.error(t("failed to log in"));
         }
         setLoading(false);
     }
@@ -37,12 +40,12 @@ export default function SignIn() {
                 <div className='py-10 lg:w-3/6 lg:px-10'>
                     <div className='lg:w-[60%] mx-auto'>
                         <h1 className='text-[#FF8A57] text-[30px] font-bold mb-6 md:text-6xl md:mb-14 lg:text-5xl xl:text-7xl'>
-                            sign-in
+                            {t("sign-in")}
                         </h1>
                         <form>
                             <input
                                 type='email'
-                                placeholder='email'
+                                placeholder={t("email")}
                                 value={email}
                                 required
                                 onChange={(e) => setEmail(e.target.value)}
@@ -51,7 +54,7 @@ export default function SignIn() {
 
                             <input
                                 type='password'
-                                placeholder='password'
+                                placeholder={t("password")}
                                 value={password}
                                 required
                                 onChange={(e) => setPassword(e.target.value)}
@@ -63,11 +66,11 @@ export default function SignIn() {
                                     disabled={loading}
                                     className='mt-7 mb-5 w-[45%] p-0'
                                 >
-                                    sign-in
+                                    {t("sign-in")}
                                 </Button>
                                 <Button className='mt-7 mb-5 w-[50%] p-0'>
                                     <Link href='/signin/forgotPassword'>
-                                        Forgot Passowrd
+                                        {t("forgot password")}
                                     </Link>
                                 </Button>
                             </div>
@@ -76,24 +79,26 @@ export default function SignIn() {
                         <div className='relative flex items-center w-[80%] mx-auto md:w-[100%] lg:w-full'>
                             <div className='flex-grow border-t border-[#a7b8c4]'></div>
                             <span className='flex-shrink mx-4 text-[#a7b8c4]'>
-                                or
+                                {t("or")}
                             </span>
                             <div className='flex-grow border-t border-[#a7b8c4]'></div>
                         </div>
                         <h3 className='text-center text-[#647581] mt-1'>
-                            sign-in with
+                            {t("sign-in with")}
                         </h3>
                         {/* sign up with socials */}
                         <div className='flex justify-center gap-2 mt-7 w-[80%] mx-auto md:w-[100%] lg:w-full'>
-                            <GoogleButton>google</GoogleButton>
-                            <FacebookButton>facebook</FacebookButton>
-                            <TwitterButton>twitter</TwitterButton>
+                            <GoogleButton>{t("google")}</GoogleButton>
+                            <FacebookButton>{t("facebook")}</FacebookButton>
+                            <TwitterButton>{t("twitter")}</TwitterButton>
                         </div>
                         <h2 className='mt-5 text-[#647581]'>
-                            dont have an account
+                            {t("dont have an account")}
                         </h2>
                         <Link href='/signup' className='block'>
-                            <Button className='mt-3 py-1 px-10'>sign up</Button>
+                            <Button className='mt-3 py-1 px-10'>
+                                {t("sign-up")}
+                            </Button>
                         </Link>
                     </div>
                 </div>
@@ -101,4 +106,13 @@ export default function SignIn() {
             </div>
         </Layout>
     );
+}
+
+export async function getStaticProps({ locale }) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ["common", "sign"])),
+            // Will be passed to the page component as props
+        },
+    };
 }
