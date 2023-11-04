c3e4909b44f854594b4ca6aba09cfa84de06710b
benarba mohammed <tewfikhotspur@gmail.com>
2023-11-04 10:30:57 +0100

    feat(authcontext): implement signin function and auth state to the context

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,6 @@
-import { createContext, useContext } from "react";
+import { auth } from "@/util/firebase";
+import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const AuthContext = createContext();
 
@@ -7,5 +9,24 @@ export function useAuth() {
 }
 
 export function AuthProvider({ children }) {
-    return <AuthContext.Provider>{children}</AuthContext.Provider>;
+    const [currentUser, setCurrentUser] = useState();
+    // sign in function
+    function signin(email, password) {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    // user State function
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setCurrentUser(user);
+        });
+        return unsubscribe;
+    }, []);
+    const value = {
+        currentUser,
+        signin,
+    };
+
+    return (
+        <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+    );
 }
