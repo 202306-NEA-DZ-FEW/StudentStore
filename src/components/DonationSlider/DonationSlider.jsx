b6f7c28200fb70ed67e874219c05bc96ab4ce7de
hocine1212 <hocine20992@gmail.com>
2023-11-05 19:04:44 +0100

    feat(donationslider.jsx): create DonationSlider component using swiper js library and tailwind CSS
    fix #60

--- src/components/DonationSlider/DonationSlider.jsx
+++ src/components/DonationSlider/DonationSlider.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import SwiperCore, { Autoplay } from "swiper/core";
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -6,7 +7,7 @@ import "swiper/swiper-bundle.min.css";
 const DonationSlider = () => {
     SwiperCore.use([Autoplay]);
     return (
-        <div className='carousel w-full md:w-[60%] mx-auto relative'>
+        <div className='carousel w-[90%] md:w-[70%] mx-auto relative'>
             <Swiper
                 slidesPerView={1}
                 autoplay={{
@@ -14,9 +15,51 @@ const DonationSlider = () => {
                 }}
                 loop={true}
             >
-                <SwiperSlide>slide 1</SwiperSlide>
-                <SwiperSlide>slide 2</SwiperSlide>
-                <SwiperSlide>slide 3</SwiperSlide>
+                <SwiperSlide>
+                    <div
+                        id='slide1'
+                        className='carousel-item relative h-[70%] w-full'
+                    >
+                        <Image
+                            src='/images/donation1.jpg'
+                            className='mx-auto '
+                            layout='responsive'
+                            width={700}
+                            height={700}
+                            alt='donation image'
+                        />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div
+                        id='slide2'
+                        className='carousel-item relative h-[70%] w-full'
+                    >
+                        <Image
+                            src='/images/donation2.jpg'
+                            className='mx-auto'
+                            width={700}
+                            height={700}
+                            layout='responsive'
+                            alt='donation image'
+                        />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div
+                        id='slide3'
+                        className='carousel-item relative h-[70%] w-full'
+                    >
+                        <Image
+                            src='/images/donation3.jpg'
+                            className='mx-auto'
+                            width={700}
+                            height={700}
+                            layout='responsive'
+                            alt='donation image'
+                        />
+                    </div>
+                </SwiperSlide>
                 ...
             </Swiper>
         </div>
