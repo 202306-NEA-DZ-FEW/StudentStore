946d4b39ccca72ec65b260ea2b7a8fadfac53c25
Katia Ghezali <ghezalikatia@hotmail.com>
2023-11-05 19:08:06 +0100

    feat(side bar): add the styling
    Side Bar #65

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -9,9 +9,7 @@ import { BiLogOut } from "react-icons/bi";
 
 const SideBar = () => {
     const [open, setOpen] = useState(false);
-
     const [selectedLink, setSelectedLink] = useState(null);
-
     const [userInfo, setUserInfo] = useState(null);
 
     useEffect(() => {
@@ -32,35 +30,70 @@ const SideBar = () => {
         fetchUserInfo();
     }, []);
 
+    const handleLinkClick = (link) => {
+        setSelectedLink(link);
+        // Handle additional logic if needed for the clicked link
+    };
+
+    const handleLogout = () => {
+        // Handle logout logic
+    };
+
     return (
-        <div className='bg-gray-200 w-64 min-h-screen p-4 flex flex-col'>
-            {userInfo && (
-                <>
-                    <div className='mb-4'>
-                        <img
-                            src={userInfo.photo}
-                            alt='User'
-                            className='w-16 h-16 rounded-full mb-2'
-                        />
-                        <h2 className='text-xl font-bold'>{`${userInfo.name} ${userInfo.surname}`}</h2>
-                        <p className='text-gray-600'>{userInfo.email}</p>
+        <div
+            className={`bg-gray-200 w-64 min-h-screen p-4 text-[#585785] flex flex-col ${
+                open ? "sm:w-20" : "sm:w-64"
+            }`}
+        >
+            <div className='flex flex-col items-center mt-20 mb-4'>
+                <img
+                    src={userInfo ? userInfo.photo : ""}
+                    alt='User'
+                    className='w-25 h-25 rounded-full mb-2'
+                />
+                {userInfo && (
+                    <div className='text-center '>
+                        <h2 className='text-xl text-[#585785] font-bold mb-1'>{`${userInfo.name} ${userInfo.surname}`}</h2>
+                        <p className='text-[#585785] mb-2'>{userInfo.email}</p>
+                        <p className='text-[#585785]'>{`${userInfo.address.city}, ${userInfo.address.country}`}</p>
                     </div>
-                    <div className='mb-4'>
-                        <p>{` ${userInfo.address.city}, ${userInfo.address.country}`}</p>
-                    </div>
-                    <div className='flex flex-col'>
-                        <a href='#' className='text-blue-500 mb-2'>
-                            Edit Profile
-                        </a>
-                        <a href='#' className='text-blue-500 mb-2'>
-                            My Listings
-                        </a>
-                        <a href='#' className='text-blue-500 mb-2'>
-                            My Orders
-                        </a>
-                    </div>
-                </>
-            )}
+                )}
+            </div>
+            <div className='flex flex-col items-center space-y-4'>
+                <div
+                    className={`text-[#585785] text-lg cursor-pointer font-semibold ${
+                        selectedLink === "EditProfile" ? "text-blue-700" : ""
+                    }`}
+                    onClick={() => handleLinkClick("EditProfile")}
+                >
+                    <span className='mr-2'>✏️</span>Edit Profile
+                </div>
+                <div
+                    className={`text-[#585785]text-lg cursor-pointer font-semibold ${
+                        selectedLink === "MyListings" ? "text-blue-700" : ""
+                    }`}
+                    onClick={() => handleLinkClick("MyListings")}
+                >
+                    <span className='mr-2'>📋</span>My Listings
+                </div>
+                <div
+                    className={`text-[#585785] text-lg cursor-pointer font-semibold ${
+                        selectedLink === "MyOrders" ? "text-blue-700" : ""
+                    }`}
+                    onClick={() => handleLinkClick("MyOrders")}
+                >
+                    <span className='mr-2'>📦</span>My Orders
+                </div>
+            </div>
+            <div className='mt-auto mb-4'>
+                <div
+                    className='flex items-center text-red-500 text-lg cursor-pointer'
+                    onClick={handleLogout}
+                >
+                    <BiLogOut className='mr-2' />
+                    Logout
+                </div>
+            </div>
         </div>
     );
 };
