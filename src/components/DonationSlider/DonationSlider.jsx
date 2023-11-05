96949ed9e200694fbb0811b8833929871fdfb310
hocine1212 <hocine20992@gmail.com>
2023-11-05 12:58:09 +0100

    feat(donationslider.jsx): install Swiper Library to use it to enhance the slider and add images

---
+++ src/components/DonationSlider/DonationSlider.jsx
@@ -0,0 +1,7 @@
+const DonationSlider = () => {
+    return (
+        <div className='carousel w-full md:w-[60%] mx-auto relative'>hello</div>
+    );
+};
+
+export default DonationSlider;
--- src/pages/donations/index.jsx
+++ src/pages/donations/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import DonationSlider from "@/components/DonationSlider/DonationSlider";
+
 function donations() {
-    return <div>donations</div>;
+    return (
+        <div className='p-4 flex justify-center'>
+            <DonationSlider />
+        </div>
+    );
 }
 
 export default donations;
