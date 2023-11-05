01122e703bc281b549466543e14c9b19ddbebb62
hocine1212 <hocine20992@gmail.com>
2023-11-05 18:46:56 +0100

    fix(donationslider.jsx): downgrade swiper js library due to a known bug in the recent releases

--- src/components/DonationSlider/DonationSlider.jsx
+++ src/components/DonationSlider/DonationSlider.jsx
@@ -1,6 +1,25 @@
+import SwiperCore, { Autoplay } from "swiper/core";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/swiper-bundle.min.css";
+
 const DonationSlider = () => {
+    SwiperCore.use([Autoplay]);
     return (
-        <div className='carousel w-full md:w-[60%] mx-auto relative'>hello</div>
+        <div className='carousel w-full md:w-[60%] mx-auto relative'>
+            <Swiper
+                slidesPerView={1}
+                autoplay={{
+                    delay: 2500,
+                }}
+                loop={true}
+            >
+                <SwiperSlide>slide 1</SwiperSlide>
+                <SwiperSlide>slide 2</SwiperSlide>
+                <SwiperSlide>slide 3</SwiperSlide>
+                ...
+            </Swiper>
+        </div>
     );
 };
 
