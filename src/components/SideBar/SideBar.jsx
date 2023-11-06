88479980c04820af80f7ac9b3ed1b1a2445e7fc8
Katia Ghezali <ghezalikatia@hotmail.com>
2023-11-06 12:29:16 +0100

    style(side bar): add css classes and fonts
    style #65

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -4,8 +4,9 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import { collection, doc, getDoc } from "firebase/firestore";
 import { db } from "../../util/firebase.js";
-
+import { FiEdit3 } from "react-icons/fi";
 import { BiLogOut } from "react-icons/bi";
+import { BsClipboard2Fill, BsFillBoxSeamFill } from "react-icons/bs";
 
 const SideBar = () => {
     const [open, setOpen] = useState(false);
@@ -45,44 +46,53 @@ const SideBar = () => {
                 open ? "sm:w-20" : "sm:w-64"
             }`}
         >
-            <div className='flex flex-col items-center mt-20 mb-4'>
+            <div className='flex flex-col items-center mt-20 mb-4 space-y-4'>
                 <img
                     src={userInfo ? userInfo.photo : ""}
                     alt='User'
-                    className='w-25 h-25 rounded-full mb-2'
+                    className='w-25 h-25 rounded-full mb-2 '
                 />
                 {userInfo && (
                     <div className='text-center '>
                         <h2 className='text-xl text-[#585785] font-bold mb-1'>{`${userInfo.name} ${userInfo.surname}`}</h2>
                         <p className='text-[#585785] mb-2'>{userInfo.email}</p>
-                        <p className='text-[#585785]'>{`${userInfo.address.city}, ${userInfo.address.country}`}</p>
+                        <p className='text-[#585785] mb-4 '>{`${userInfo.address.city}, ${userInfo.address.country}`}</p>
                     </div>
                 )}
             </div>
-            <div className='flex flex-col items-center space-y-4'>
+            <div className='flex flex-col items-center space-y-10'>
                 <div
-                    className={`text-[#585785] text-lg cursor-pointer font-semibold ${
-                        selectedLink === "EditProfile" ? "text-blue-700" : ""
+                    className={`flex items-center text-[#585785] text-2xl cursor-pointer font-semibold ${
+                        selectedLink === "EditProfile" ? "bg-[#90EEE1]" : ""
                     }`}
                     onClick={() => handleLinkClick("EditProfile")}
                 >
-                    <span className='mr-2'>✏️</span>Edit Profile
+                    <span className='mr-2'>
+                        <FiEdit3 />
+                    </span>
+                    Edit Profile
                 </div>
                 <div
-                    className={`text-[#585785]text-lg cursor-pointer font-semibold ${
-                        selectedLink === "MyListings" ? "text-blue-700" : ""
+                    className={`flex items-center text-[#585785] text-2xl cursor-pointer font-semibold ${
+                        selectedLink === "MyListings" ? "bg-[#90EEE1]" : ""
                     }`}
                     onClick={() => handleLinkClick("MyListings")}
                 >
-                    <span className='mr-2'>📋</span>My Listings
+                    <span className='mr-2'>
+                        <BsClipboard2Fill />
+                    </span>
+                    My Listings
                 </div>
                 <div
-                    className={`text-[#585785] text-lg cursor-pointer font-semibold ${
-                        selectedLink === "MyOrders" ? "text-blue-700" : ""
+                    className={`flex items-center text-[#585785] text-2xl cursor-pointer font-semibold ${
+                        selectedLink === "MyOrders" ? "bg-[#90EEE1]" : ""
                     }`}
                     onClick={() => handleLinkClick("MyOrders")}
                 >
-                    <span className='mr-2'>📦</span>My Orders
+                    <span className='mr-2'>
+                        <BsFillBoxSeamFill />
+                    </span>
+                    My Orders
                 </div>
             </div>
             <div className='mt-auto mb-4'>
