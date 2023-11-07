517599e0786041c10be8cec4c6f1494fe89ddcce
hocine1212 <hocine20992@gmail.com>
2023-11-07 12:00:27 +0100

    feat(donations page): implement English translation for the Donation Page
    fix #71

--- src/components/PricingCard/PricingCard.jsx
+++ src/components/PricingCard/PricingCard.jsx
@@ -1,20 +1,19 @@
-import React from "react";
 import { toast } from "react-toastify";
 
 import Button from "../Buttons/Button";
 
-const PricingCard = ({ title, amount }) => {
+const PricingCard = ({ title, amount, t }) => {
     const handleClick = () => {
-        toast.success(`Thank you for donating ${amount}$`);
+        toast.success(t("thank you for donating ") + amount + "$");
     };
     return (
-        <div className='p-8  md:p-4 text-black text-center border rounded-lg shadow-md bg-white'>
+        <div className='p-8 md:p-4 text-black text-center border rounded-lg shadow-md bg-white'>
             <h2 className='text-xl   font-semibold'>{title}</h2>
             <p className=' text-6xl my-2 font-semibold flex justify-center'>
                 <span className='text-3xl self-center'>$</span>
                 {amount}
             </p>
-            <Button onClick={handleClick}>Donate</Button>
+            <Button onClick={handleClick}>{t("donate")}</Button>
         </div>
     );
 };
