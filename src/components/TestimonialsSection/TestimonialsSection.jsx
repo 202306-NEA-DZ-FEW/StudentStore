261d50057317ca0196b5c97ed4bdfe6e6bb2dbe4
benarba mohammed <tewfikhotspur@gmail.com>
2023-11-09 18:53:16 +0100

    feat(testimonialssection): add styling to the testimonialsSection

--- src/components/TestimonialsSection/TestimonialsSection.jsx
+++ src/components/TestimonialsSection/TestimonialsSection.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import {
+    IoIosArrowDroprightCircle,
+    IoIosArrowDropleftCircle,
+} from "react-icons/io";
 import { FaQuoteRight } from "react-icons/fa";
 import data from "@/util/data";
 import Image from "next/image";
@@ -27,8 +30,8 @@ function TestimonialsSection() {
     }, [index]);
 
     return (
-        <section className='w-[90vw] my-[5rem] mx-auto'>
-            <div className='mx-auto mt-0 w-[80vw] h-[450px] max-w-[800px] text-center relative flex overflow-hidden'>
+        <section className='py-6 mx-auto'>
+            <div className='mx-auto mt-0 w-[80vw] lg:w-[65vw] h-[350px] lg:[300px]max-w-[800px] text-center relative flex items-center overflow-hidden'>
                 {people.map((person, personIndex) => {
                     const { id, image, name, quote } = person;
 
@@ -45,7 +48,7 @@ function TestimonialsSection() {
 
                     return (
                         <article
-                            className={`${position} absolute top-0 left-0 w-full py-3 bg-white`}
+                            className={`${position} absolute h-auto w-full py-3 rounded-lg bg-white`}
                             key={id}
                         >
                             <Image
@@ -53,32 +56,32 @@ function TestimonialsSection() {
                                 alt={name}
                                 width={100}
                                 height={100}
-                                className='object-cover h-16 w-16 mx-auto block border-4 border-purple-500 rounded-full my-4'
+                                className='object-cover h-16 w-16 mx-auto block border-2 border-[#7874F2] rounded-full my-4'
                             />
 
-                            <h4 className='uppercase mb-[0.25rem] text-[1rem]'>
+                            <h4 className='uppercase mb-[0.25rem] text-[1rem] font-bold text-[#7874F2]'>
                                 {name}
                             </h4>
-                            <p className='max-w-[20rem] lg:max-w-[30rem] lg:mt-8 mx-auto  leading-loose'>
+                            <p className='max-w-[15rem] sm:max-w-[25rem] md:max-w-[30rem] lg:max-w-[35rem] xl:max-w-[40rem] mx-auto  leading-loose'>
                                 {quote}
                             </p>
-                            <div className='flex justify-center text-purple-500'>
-                                <FaQuoteRight className='text-2xl' />
+                            <div className='flex justify-center text-[#7874F2]'>
+                                <FaQuoteRight className='text-3xl mt-2' />
                             </div>
                         </article>
                     );
                 })}
                 <button
-                    className='absolute top-[200px] -translate-y-1/2 w-[1.25rem] h-[1.25rem] grid place-items-center text-[1rem] cursor-pointer left-0'
+                    className='absolute top-[50%] -translate-y-1/2 w-[1.25rem] h-[1.25rem] grid place-items-center text-[1rem] cursor-pointer text-gray-400 hover:text-[#7874F2] left-0'
                     onClick={() => setIndex(index - 1)}
                 >
-                    <FiChevronLeft className='text-3xl' />
+                    <IoIosArrowDropleftCircle className='text-3xl' />
                 </button>
                 <button
-                    className='absolute top-[200px] -translate-y-1/2 w-[1.25rem] h-[1.25rem] grid place-items-center text-[1rem] cursor-pointer right-2'
+                    className='absolute top-[50%] -translate-y-1/2 w-[1.25rem] h-[1.25rem] grid place-items-center text-[1rem] cursor-pointer text-gray-400 hover:text-[#7874F2] right-3'
                     onClick={() => setIndex(index + 1)}
                 >
-                    <FiChevronRight className='text-3xl' />
+                    <IoIosArrowDroprightCircle className='text-3xl' />
                 </button>
             </div>
         </section>
