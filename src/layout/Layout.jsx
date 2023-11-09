a1918ac3fd63f747adcc3384697cd5e39b96e5ec
hocine1212 <hocine20992@gmail.com>
2023-11-09 18:12:10 +0100

    fix(listing page): fix styling issues on the listing page
    fix #75

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,13 +1,11 @@
 import * as React from "react";
 
-import Navbar from "../components/NavBar/NavBar";
-
 export default function Layout({ children }) {
     // Put Header or Footer around the children element
     // Example
     return (
         <>
-            <Navbar />
+            {/* <Navbar /> */}
             {children}
             {/* <Footer /> */}
         </>
