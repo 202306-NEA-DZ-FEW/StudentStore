ef67258fceb9a3d6dc0729539c4782d879c83827
Katia Ghezali <ghezalikatia@hotmail.com>
2023-11-10 17:20:34 +0100

    test(product details): add tests
    test #81

--- src/components/ProductDetails/__test__/ProductDetails.test.js
+++ src/components/ProductDetails/__test__/ProductDetails.test.js
@@ -2,6 +2,12 @@ import renderer from "react-test-renderer";
 
 import ProductDetails from "../ProductDetails";
 
+jest.mock("../../../util/firebase", () => {
+    return {
+        initializeApp: jest.fn(),
+    };
+});
+
 it("renders correctly", () => {
     const tree = renderer.create(<ProductDetails />).toJSON();
     expect(tree).toMatchSnapshot();
