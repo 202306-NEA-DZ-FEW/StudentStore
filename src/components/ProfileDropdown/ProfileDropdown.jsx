9b5c50e2326768af9414bcb2e01466063c7ad07f
benarba mohammed <tewfikhotspur@gmail.com>
2023-11-15 21:42:53 +0100

    fix(profile dropdown): fix current userState when the user is loged out

--- src/components/ProfileDropdown/ProfileDropdown.jsx
+++ src/components/ProfileDropdown/ProfileDropdown.jsx
@@ -23,59 +23,70 @@ function ProfileDropdown() {
             toast.error("failed to log out");
         }
     }
+    console.log(currentUser);
     return (
-        <div className='dropdown dropdown-end'>
-            <label
-                tabIndex={0}
-                onClick={toggleDropdown}
-                className='btn btn-ghost hover:bg-slate-200 btn-circle avatar'
-            >
-                <div className='w-8 rounded-full'>
-                    <Image
-                        src={
-                            currentUser
-                                ? currentUser.photoURL
-                                : "/images/profile.jpg"
-                        }
-                        width={20}
-                        height={20}
-                        alt='profile-pic'
-                    />
+        <>
+            {currentUser ? (
+                <div className='dropdown dropdown-end'>
+                    <label
+                        tabIndex={0}
+                        onClick={toggleDropdown}
+                        className='btn btn-ghost hover:bg-slate-200 btn-circle avatar'
+                    >
+                        <div className='w-8 rounded-full'>
+                            <Image
+                                src={
+                                    currentUser.photoURL ||
+                                    "/images/profile.jpg"
+                                }
+                                width={20}
+                                height={20}
+                                alt='profile-pic'
+                            />
+                        </div>
+                    </label>
+                    {isDropdownOpen && (
+                        <ul
+                            tabIndex={0}
+                            className='mt-1 z-[1] px-1 py-2 shadow menu menu-sm dropdown-content  text-black bg-[#F1F6FA] rounded-md w-22'
+                        >
+                            <li>
+                                <Link
+                                    href='#'
+                                    className='text-[#7874F2] font-semibold justify-between hover:text-grey'
+                                >
+                                    Profile
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    href='#'
+                                    className='text-[#7874F2] font-semibold  hover:text-grey'
+                                >
+                                    Listings
+                                </Link>
+                            </li>
+                            <li>
+                                <button
+                                    className='text-[#7874F2] font-semibold   hover:text-grey'
+                                    onClick={handleLogout}
+                                >
+                                    Logout
+                                </button>
+                            </li>
+                        </ul>
+                    )}
+                    <ToastContainer />
                 </div>
-            </label>
-            {isDropdownOpen && (
-                <ul
-                    tabIndex={0}
-                    className='mt-1 z-[1] px-1 py-2 shadow menu menu-sm dropdown-content  text-black bg-[#F1F6FA] rounded-md w-22'
+            ) : (
+                <Link
+                    className='bg-[#585785] py-1 px-1 rounded-md text-white hover:bg-[#6d6d9a] transition duration-300'
+                    href='/signin'
                 >
-                    <li>
-                        <Link
-                            href='#'
-                            className='text-[#7874F2] font-semibold justify-between hover:text-grey'
-                        >
-                            Profile
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href='#'
-                            className='text-[#7874F2] font-semibold  hover:text-grey'
-                        >
-                            Listings
-                        </Link>
-                    </li>
-                    <li>
-                        <button
-                            className='text-[#7874F2] font-semibold   hover:text-grey'
-                            onClick={handleLogout}
-                        >
-                            Logout
-                        </button>
-                    </li>
-                </ul>
+                    Sign In
+                </Link>
             )}
-            <ToastContainer />
-        </div>
+        </>
     );
 }
 
