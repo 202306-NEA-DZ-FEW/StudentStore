60c18750f9cc76d8784f160c7a0e43d8912f40e2
benarba mohammed <tewfikhotspur@gmail.com>
2023-11-16 23:01:30 +0100

    feat(filter): create filter component and create filter test

---
+++ src/components/Filter/__test__/Filter.test.js
@@ -0,0 +1,7 @@
+import renderer from "react-test-renderer";
+import Filter from "../Filter";
+
+it("renders correctly", () => {
+    const tree = renderer.create(<Filter />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
