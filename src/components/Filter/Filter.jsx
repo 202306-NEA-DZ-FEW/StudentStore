b7ad4fcd881f9c599a898995d485b29ba2d7c71b
benarba mohammed <tewfikhotspur@gmail.com>
2023-11-17 12:36:25 +0100

    feat(filter): implement price filter functionality

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,3 +1,34 @@
-export default function Filter() {
-    return <div>Filter</div>;
+export default function Filter({
+    minPrice,
+    maxPrice,
+    handleMinPriceChange,
+    handleMaxPriceChange,
+}) {
+    return (
+        <div className='w-1/4'>
+            <div>
+                <div>
+                    <h2>Price</h2>
+                </div>
+                <div className='ml-4'>
+                    <div className='flex space-x-2 justify-center my-2'>
+                        <input
+                            type='number'
+                            placeholder='Min'
+                            className='w-1/2 h-6 border rounded-md px-2 focus:outline-none'
+                            value={minPrice}
+                            onChange={handleMinPriceChange}
+                        />
+                        <input
+                            type='number'
+                            placeholder='Max'
+                            className='w-1/2 h-6 border rounded-md px-2 focus:outline-none'
+                            value={maxPrice}
+                            onChange={handleMaxPriceChange}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/components/ProductsContainer/ProductsContainer.jsx
+++ src/components/ProductsContainer/ProductsContainer.jsx
@@ -1,9 +1,64 @@
+import { useEffect, useRef, useState } from "react";
+import Filter from "../Filter/Filter";
 import ProductsList from "../ProductsList/ProductsList";
 
 export default function ProductsContainer({ products }) {
+    const [filteredProducts, setFilteredProducts] = useState([]);
+    const [minPrice, setMinPrice] = useState("");
+    const [maxPrice, setMaxPrice] = useState("");
+    // debounce functionality
+    const debounceTimeout = useRef(null);
+    const debounce = (func, delay) => {
+        return function (...args) {
+            if (debounceTimeout.current) {
+                clearTimeout(debounceTimeout.current);
+            }
+
+            debounceTimeout.current = setTimeout(() => {
+                func(...args);
+            }, delay);
+        };
+    };
+    //  min price change function
+    const handleMinPriceChange = (e) => {
+        const newMinPrice = e.target.value;
+        setMinPrice(newMinPrice);
+        debounce(filterProducts, 500)(newMinPrice, maxPrice);
+    };
+    // max price change function
+    const handleMaxPriceChange = (e) => {
+        const newMaxPrice = e.target.value;
+        setMaxPrice(newMaxPrice);
+        debounce(filterProducts, 500)(minPrice, newMaxPrice);
+    };
+    // filter price functions
+    const filterProducts = (min, max) => {
+        const filtered = products.filter((product) => {
+            const price = parseFloat(product.price);
+            const minPriceValue = parseFloat(min);
+            const maxPriceValue = parseFloat(max);
+            return (
+                isNaN(price) ||
+                ((isNaN(minPriceValue) || price >= minPriceValue) &&
+                    (isNaN(maxPriceValue) || price <= maxPriceValue))
+            );
+        });
+
+        setFilteredProducts(filtered);
+    };
+    // using this useEffect to diplay all product at the firt render
+    useEffect(() => {
+        setFilteredProducts(products);
+    }, [products]);
     return (
-        <div>
-            <ProductsList products={products} />
+        <div className='flex'>
+            <Filter
+                minPrice={minPrice}
+                maxPrice={maxPrice}
+                handleMinPriceChange={handleMinPriceChange}
+                handleMaxPriceChange={handleMaxPriceChange}
+            />
+            <ProductsList products={filteredProducts} />
         </div>
     );
 }
