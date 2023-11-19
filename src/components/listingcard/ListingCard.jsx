6c9e3b45dd445b576b1ccb527da78dba7709faa6
Katia Ghezali <ghezalikatia@hotmail.com>
2023-11-20 00:44:28 +0100

    feat(listing product): handle deleting products from listing pages
    #119

--- src/components/listingcard/ListingCard.jsx
+++ src/components/listingcard/ListingCard.jsx
@@ -1,8 +1,38 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { FaTrash } from "react-icons/fa";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "@/util/firebase";
 
 function ListingCard({ product }) {
+    const [isDeleting, setIsDeleting] = useState(false);
+
+    const handleDelete = async () => {
+        const confirmDelete = window.confirm(
+            "Are you sure you want to delete this product?"
+        );
+
+        if (confirmDelete) {
+            try {
+                setIsDeleting(true);
+
+                const productRef = doc(db, "products", product.id);
+                await deleteDoc(productRef);
+
+                toast.success("Product deleted successfully!");
+            } catch (error) {
+                console.error("Error deleting product:", error);
+                toast.error(`Failed to delete product: ${error.message}`);
+            } finally {
+                setIsDeleting(false);
+            }
+        }
+    };
+
     return (
         <div className='w-56 bg-white shadow-md rounded-xl duration-500  hover:shadow-xl'>
             <Link href='/'>
@@ -47,21 +77,8 @@ function ListingCard({ product }) {
                         {product?.price}$
                     </p>
                     <div className='ml-auto'>
-                        <button className='hover:text-purple-700'>
-                            <svg
-                                className=''
-                                xmlns='http://www.w3.org/2000/svg'
-                                width='26'
-                                height='26'
-                                fill='currentColor'
-                                viewBox='0 0 16 16'
-                            >
-                                <path
-                                    fillRule='evenodd'
-                                    d='M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z'
-                                />
-                                <path d='M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z' />
-                            </svg>
+                        <button onClick={handleDelete} className='text-red-500'>
+                            <FaTrash />
                         </button>
                     </div>
                 </div>
