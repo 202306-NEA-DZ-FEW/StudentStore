0421e9d9bae0ac2d447cfb504ff4fe15a9467511
benarba mohammed <tewfikhotspur@gmail.com>
2023-11-20 00:05:36 +0100

    feat(latestproducts): add fetching functionality to the latestproducts components

--- src/components/LatestProducts/LatestProducts.jsx
+++ src/components/LatestProducts/LatestProducts.jsx
@@ -1,3 +1,28 @@
+import { db } from "@/util/firebase";
+import { collection, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import ProductCard from "../ProductCard/ProductCard";
+
 export default function LatestProducts() {
-    return <div>LatestProducts</div>;
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        const colRef = collection(db, "products");
+        onSnapshot(colRef, (snapshot) => {
+            let products = [];
+            snapshot.docs.forEach((doc) =>
+                products.push({ ...doc.data(), id: doc.id })
+            );
+            let latestProducts = products.slice(1, 9);
+            setProducts(latestProducts);
+        });
+    }, []);
+    return (
+        <div>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:w-[70%] mx-auto gap-4 place-items-center'>
+                {products?.map((product) => {
+                    return <ProductCard key={product.id} product={product} />;
+                })}
+            </div>
+        </div>
+    );
 }
--- src/components/LatestProducts/__test__/LatestProducts.test.js
+++ src/components/LatestProducts/__test__/LatestProducts.test.js
@@ -1,6 +1,12 @@
 import renderer from "react-test-renderer";
 import LatestProducts from "../LatestProducts";
 
+jest.mock("../../../util/firebase", () => {
+    return {
+        initializeApp: jest.fn(),
+    };
+});
+
 it("renders correctly", () => {
     const tree = renderer.create(<LatestProducts />).toJSON();
     expect(tree).toMatchSnapshot();
