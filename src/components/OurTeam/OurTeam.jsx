753b1ddb35ba5cac25308e19b705b974d39ecc36
Sami <s.babouche@esi-sba.dz>
2023-11-22 00:07:54 +0100

    feat(ourteam.jsx): create ourteam component
    #130

---
+++ src/components/OurTeam/OurTeam.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import Head from "next/head";
+const OurTeam = ({ data }) => {
+    return (
+        <section className='p-4'>
+            <div className='text-center  text-black'>
+                {" "}
+                <h1 className='text-4xl font-bold text-[#585785]  '>
+                    {" "}
+                    MEET OUR TEAM
+                </h1>
+                <p className='text-xl leading-relaxed  '>
+                    {" "}
+                    Were a dynamic group of individuals who are passionate about
+                    what we do and dedicated to delivering the best results for
+                    our clients.
+                </p>{" "}
+            </div>
+
+            <div className='mx-auto lg:flex-row flex-col flex lg:space-x-20 lg:space-y-0 space-y-20 items-center p-12 '>
+                {data?.map((data, key) => {
+                    const { id, image, name, title, bio, github, linkedin } =
+                        data;
+                    return (
+                        <div
+                            key={id}
+                            className='group relative w-56 h-56 bg-[#c1464627] drop-shadow-lg p-8 rounded-full shadow-md transition-all duration-500 hover:rounded-2xl hover:h-50'
+                        >
+                            <div className='img group-hover:h-40 w-full h-full'>
+                                <img
+                                    src='/Hocine.jpg'
+                                    alt='Profile'
+                                    className='w-full h-full rounded-full transition duration-500 transform group-hover:-translate-y-20 group-hover:rounded-xl group-hover:shadow-xl '
+                                />
+                            </div>
+                            <div className='caption text-center transform -translate-y-20 opacity-0 delay-200  transition-all duration-200 group-hover:opacity-100 '>
+                                <div className='card-content  flex-col  '>
+                                    <div>
+                                        {" "}
+                                        <h4 className=' break-normal font-bold text-lg  text-[#585785] '>
+                                            {name}
+                                        </h4>
+                                        <h2 className='text-md'> {title} </h2>
+                                    </div>
+
+                                    <div className='social-media flex justify-center space-x-4 '>
+                                        <Link
+                                            className='cursor-pointer hover:text-[#000000]'
+                                            href={`${github}`}
+                                            target='_blank'
+                                        >
+                                            <FaGithub size={25} />
+                                        </Link>
+                                        <Link
+                                            className=' cursor-pointer   hover:text-[#0e76a8]'
+                                            href={`${linkedin}`}
+                                            target='_blank'
+                                        >
+                                            <FaLinkedin size={25} />
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </section>
+    );
+};
+
+export default OurTeam;
--- src/pages/aboutus/index.jsx
+++ src/pages/aboutus/index.jsx
@@ -1,7 +1,43 @@
+import OurTeam from "@/components/OurTeam/OurTeam";
 import React from "react";
 
+const data = [
+    {
+        id: 1,
+        image: "/Hocine.jpg",
+        name: "Hocine Benouddane",
+        title: "Front-end Developer",
+        bio: "An enthusiastic front-end developer with a deep love for coding and an insatiable curiosity for exploring and mastering new technologies.",
+        github: "https://github.com/hocine1212",
+        linkedin: "www.linkedin.com/in/hocine12",
+    },
+    {
+        id: 2,
+        image: "/Event.jpg",
+        title: "Front-end Developer",
+        name: "Benarba Tewfik",
+        bio: "Passionate frontend developer from Algeria, always eager to embrace new challenges and learn the latest in web development.",
+        github: "https://github.com/Ben-Tewfik",
+        linkedin: "https://www.linkedin.com/in/mohammed-tewfik-benarba/",
+    },
+    {
+        id: 3,
+        image: "/photography.jpg",
+        title: "Khaoula AOURRA",
+        name: "Book Buddies",
+        bio: "Hey, I'm Khaoula Aourra—a finance and banking grad turned tech enthusiast! I embraced the world of front-end development through Re-coded's boot camp. Excited to fuse financial savvy with tech skills and make a splash in this dynamic industry! 🚀",
+        github: "https://github.com/khaoulasara/AOURRAkhaoula",
+        linkedin:
+            "https://www.eventbrite.com/e/discover-photography-registration-36242436102?aff=ebdssbdestsearch&keep_tld=1",
+    },
+];
 function AboutUs() {
-    return <div>AboutUs.</div>;
+    return (
+        <div>
+            AboutUs.
+            <OurTeam data={data}></OurTeam>
+        </div>
+    );
 }
 
 export default AboutUs;
