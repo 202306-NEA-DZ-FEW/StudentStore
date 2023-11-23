269716e9c8a467d678cedfc1ee74be605d2f3464
hocine1212 <hocine20992@gmail.com>
2023-11-23 20:39:39 +0100

    feat(listing page): add delete button and functionality to the listing's images

--- src/pages/listing/index.jsx
+++ src/pages/listing/index.jsx
@@ -40,6 +40,16 @@ const Listing = () => {
             const file = e.target.files[0];
             if (file) {
                 const files = e.target.files;
+                // Check if the total number of images is greater than 4
+                if (formData.pictures.length + files.length > 4) {
+                    toast.warning(
+                        t("You can only upload a maximum of 4 images."),
+                        {
+                            rtl: route.locale === "ar",
+                        }
+                    );
+                    return;
+                }
                 // reject if the image is selected twice
                 const selectedFileNames = formData.pictures.map(
                     (imgFile) => imgFile.name
@@ -95,6 +105,23 @@ const Listing = () => {
             }));
         } else {
             setFormData({ ...formData, [name]: value });
+        }
+    };
+
+    const handleImageDelete = (index) => {
+        // Remove the image file from the array
+        const updatedImageFiles = [...imageFiles];
+        updatedImageFiles.splice(index, 1);
+        setImageFiles(updatedImageFiles);
+
+        // Remove the image URL from the form data
+        const updatedImgUrl = [...formData.pictures];
+        updatedImgUrl.splice(index, 1);
+        setFormData({ ...formData, pictures: updatedImgUrl });
+        // Reset the file input value
+        const fileInput = document.getElementById("imageInput");
+        if (fileInput) {
+            fileInput.value = "";
         }
     };
 
@@ -190,6 +217,8 @@ const Listing = () => {
         }
     };
 
+    console.log(formData);
+
     useEffect(() => {
         // Scroll to top when isLoading state changes
         if (isUploading) {
@@ -225,27 +254,35 @@ const Listing = () => {
                     <div className='grid grid-cols-3 grid-rows-3 md:flex lg:grid lg:grid-rows-3  w-full sm:w-full md:w-full lg:w-1/2  justify-between  gap-4'>
                         <div
                             className={classNames(
-                                "col-span-3 row-span-3 md:grid-rows-2 items-center justify-center overflow-hidden w-full h-72 lg:h-80 md:w-[60%] lg:w-full bg-[#EEF2F4]  border rounded-md border-[#979797]",
+                                "col-span-3 row-span-3 md:grid-rows-2 items-center justify-center overflow-hidden  w-full h-72 lg:h-80 md:w-[60%] lg:w-full bg-[#EEF2F4]  border rounded-md border-[#979797]",
                                 { "empty-image-container": !imageFiles[0] }
                             )}
                         >
                             {imageFiles[0] ? (
-                                <Image
-                                    className='object-cover flex-reverse  w-full h-full'
-                                    src={imageFiles[0]}
-                                    width={1200}
-                                    height={1000}
-                                    alt='product image'
-                                />
+                                <div className='relative w-full h-72 lg:h-80 lg:w-full'>
+                                    <Image
+                                        className='object-cover flex-reverse  w-full h-full'
+                                        src={imageFiles[0]}
+                                        width={1200}
+                                        height={1000}
+                                        alt='product image'
+                                    />
+                                    <button
+                                        onClick={() => handleImageDelete(0)}
+                                        className='absolute top-2 right-2 p-2 h-4 w-4 grid place-content-center bg-red-500 rounded-full text-white cursor-pointer'
+                                    >
+                                        X
+                                    </button>
+                                </div>
                             ) : (
                                 <div className='empty-image-container'></div>
                             )}
                         </div>
                         <div className=' col-span-3 row-span-1    md:w-[40%] lg:w-full rounded-md gap-4'>
-                            <div className='flex justify-between  gap-4 md:grid md:grid-cols-2 md:grid-rows-2 sm:gap-6  lg:flex   '>
+                            <div className='flex justify-between  gap-4 md:grid md:grid-cols-2 md:grid-rows-2 sm:gap-6  lg:flex'>
                                 <div
                                     className={classNames(
-                                        "col-span-2 row-span-1 md:row-span-2 w-full  bg-[#EEF2F4] h-20 sm:h-24 md:h-40 lg:h-28  border rounded-md border-[#979797] overflow-hidden",
+                                        "col-span-2 row-span-1 md:row-span-2 w-full  bg-[#EEF2F4]   border rounded-md border-[#979797] overflow-hidden",
                                         {
                                             "empty-image-container bg-[#EEF2F4] lg:bg-[#D1DBE3]":
                                                 !imageFiles[1],
@@ -253,13 +290,23 @@ const Listing = () => {
                                     )}
                                 >
                                     {imageFiles[1] ? (
-                                        <Image
-                                            className='object-cover  w-full h-full rounded-md '
-                                            src={imageFiles[1]}
-                                            width={1200}
-                                            height={1000}
-                                            alt='product image'
-                                        />
+                                        <div className='relative w-full h-20 sm:h-24 md:h-40 lg:h-28'>
+                                            <Image
+                                                className='object-cover  w-full h-full rounded-md '
+                                                src={imageFiles[1]}
+                                                width={1200}
+                                                height={1000}
+                                                alt='product image'
+                                            />
+                                            <button
+                                                onClick={() =>
+                                                    handleImageDelete(1)
+                                                }
+                                                className='absolute top-2 right-2 p-2 h-4 w-4 grid place-content-center bg-red-500 rounded-full text-white cursor-pointer'
+                                            >
+                                                X
+                                            </button>
+                                        </div>
                                     ) : (
                                         <div className='empty-image-container'></div>
                                     )}
@@ -275,20 +322,30 @@ const Listing = () => {
                                     )}
                                 >
                                     {imageFiles[2] ? (
-                                        <Image
-                                            className='object-cover flex-reverse w-full h-full '
-                                            src={imageFiles[2]}
-                                            width={1200}
-                                            height={1000}
-                                            alt='product image'
-                                        />
+                                        <div className='relative h-20 sm:h-24 lg:h-28 '>
+                                            <Image
+                                                className='object-cover flex-reverse w-full h-full '
+                                                src={imageFiles[2]}
+                                                width={1200}
+                                                height={1000}
+                                                alt='product image'
+                                            />
+                                            <button
+                                                onClick={() =>
+                                                    handleImageDelete(2)
+                                                }
+                                                className='absolute top-2 right-2 p-2 h-4 w-4 grid place-content-center bg-red-500 rounded-full text-white cursor-pointer'
+                                            >
+                                                X
+                                            </button>
+                                        </div>
                                     ) : (
                                         <div className='empty-image-container'></div>
                                     )}
                                 </div>
                                 <div
                                     className={classNames(
-                                        "col-span-1 row-span-1 bg-[#EEF2F4] h-20 sm:h-24 lg:h-28  w-full border  rounded-md border-[#979797] overflow-hidden lg:w-full",
+                                        "col-span-1 row-span-1 bg-[#EEF2F4]  w-full h-20 sm:h-24 lg:h-28 border  rounded-md border-[#979797] overflow-hidden lg:w-full",
                                         {
                                             "empty-image-container bg-[#EEF2F4] lg:bg-[#D1DBE3]":
                                                 !imageFiles[3],
@@ -296,13 +353,23 @@ const Listing = () => {
                                     )}
                                 >
                                     {imageFiles[3] ? (
-                                        <Image
-                                            className='object-cover flex-reverse w-full h-full '
-                                            src={imageFiles[3]}
-                                            width={1200}
-                                            height={1000}
-                                            alt='product image'
-                                        />
+                                        <div className='relative h-20 sm:h-24 lg:h-28 w-full'>
+                                            <Image
+                                                className='object-cover flex-reverse w-full h-full '
+                                                src={imageFiles[3]}
+                                                width={1200}
+                                                height={1000}
+                                                alt='product image'
+                                            />
+                                            <button
+                                                onClick={() =>
+                                                    handleImageDelete(3)
+                                                }
+                                                className='absolute top-2 right-2 p-2 h-4 w-4 grid place-content-center bg-red-500 rounded-full text-white cursor-pointer'
+                                            >
+                                                X
+                                            </button>
+                                        </div>
                                     ) : (
                                         <div className='empty-image-container'></div>
                                     )}
@@ -436,6 +503,7 @@ const Listing = () => {
                                 <input
                                     type='file'
                                     onChange={handleChange}
+                                    id='imageInput'
                                     className='hidden'
                                 />
                             </label>
