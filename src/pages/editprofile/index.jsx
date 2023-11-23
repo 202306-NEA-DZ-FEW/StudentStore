fb4e8f6dcfaf8a922de0511a85c6263464b3d1b4
Katia Ghezali <ghezalikatia@hotmail.com>
2023-11-23 21:11:29 +0100

    feat(edit profile page): fix responsiveness issues
    Edit profile page #143

--- src/pages/editprofile/index.jsx
+++ src/pages/editprofile/index.jsx
@@ -1,27 +1,16 @@
-import { collection, getDocs } from "firebase/firestore";
-import Link from "next/link";
-import { useTranslation } from "next-i18next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import * as React from "react";
 import EditForm from "@/components/EditForm/EditForm";
-
 import Sidebar from "@/components/SideBar/SideBar";
 
-export default function HomePage() {
-    const { t } = useTranslation("common");
+const EditProfilePage = () => {
     return (
         <div className='flex'>
             <Sidebar />
-            <EditForm />
+            <div className='flex-1 flex justify-center items-center p-4'>
+                <EditForm />
+            </div>
         </div>
     );
-}
+};
 
-export async function getStaticProps({ locale }) {
-    return {
-        props: {
-            ...(await serverSideTranslations(locale, ["common"])),
-            // Will be passed to the page component as props
-        },
-    };
-}
+export default EditProfilePage;
