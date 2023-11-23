6603ba25f1412d7d19970ca3746a3c8e5df0a1b4
hocine1212 <hocine20992@gmail.com>
2023-11-23 23:12:06 +0100

    feat(productcard.jsx): add hover effect to the ProductCard's image

--- src/pages/listing/index.jsx
+++ src/pages/listing/index.jsx
@@ -217,6 +217,8 @@ const Listing = () => {
         }
     };
 
+    console.log(formData);
+
     useEffect(() => {
         // Scroll to top when isLoading state changes
         if (isUploading) {
