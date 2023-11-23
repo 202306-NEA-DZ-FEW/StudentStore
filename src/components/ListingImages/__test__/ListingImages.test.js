f4b1821452cc0ba9ef571e9baf2622cc4fdef3c7
hocine1212 <hocine20992@gmail.com>
2023-11-23 21:43:49 +0100

    feat(listingimages.jsx): implement the logic and update the tests of the ListingImages component

--- src/components/ListingImages/__test__/ListingImages.test.js
+++ src/components/ListingImages/__test__/ListingImages.test.js
@@ -3,6 +3,9 @@ import renderer from "react-test-renderer";
 import ListingImages from "../ListingImages";
 
 it("renders correctly", () => {
-    const tree = renderer.create(<ListingImages />).toJSON();
+    const imageFiles = ["/empty-image-container"];
+    const tree = renderer
+        .create(<ListingImages imageFiles={imageFiles} />)
+        .toJSON();
     expect(tree).toMatchSnapshot();
 });
