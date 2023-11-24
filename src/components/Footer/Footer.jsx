5f031b850825763a4da281fabd09632075c1e234
benarba mohammed <tewfikhotspur@gmail.com>
2023-11-24 22:14:37 +0100

    style(footer): modify the style for tablet and desktop screens

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -22,24 +22,27 @@ function Footer({ t }) {
     };
 
     return (
-        <footer className='' dir={`${route.locale === "ar" ? "rtl" : "ltr"}`}>
-            <div className='bg-[#32314C] flex flex-col font-semibold gap-4 p-2 md:flex-row md:justify-around'>
+        <footer
+            className='bg-[#32314C]'
+            dir={`${route.locale === "ar" ? "rtl" : "ltr"}`}
+        >
+            <div className=' md:w-full lg:w-[80vw] mx-auto flex flex-col font-semibold gap-4 p-2 md:flex-row md:justify-around'>
                 {/* for logo */}
-                <div>
+                <div className='flex justify-center items-center'>
                     {/* image container */}
-                    <div className='w-full hidden md:flex justify-center'>
+                    <div className='w-full hidden sm:flex justify-center items-center md:w-[1/5]'>
                         <Link href='/home'>
                             <Image
                                 src='/images/logo-footer.svg'
-                                width={130}
-                                height={40}
+                                width={150}
+                                height={60}
                                 alt='logo'
                             />
                         </Link>
                     </div>
                 </div>
                 {/* for links */}
-                <div className='text-white gap-2 ml-4 md:flex'>
+                <div className='text-white gap-2 ml-4 sm:flex justify-around md:w-[60%]'>
                     {/* services */}
                     <div className='flex flex-col gap-1 mb-2'>
                         <h1 className='text-[#FF8A57] font-bold'>Services</h1>
@@ -71,19 +74,26 @@ function Footer({ t }) {
                                 >
                                     {t("about_us")}
                                 </Link>
-                                <h2 className='text-[#FF8A57] mb-1'>
-                                    {t("contact_us")}
-                                </h2>
+                                <h2 className='mb-1'>{t("contact_us")}:</h2>
                                 <Link
                                     href='tel:+2130699514862'
                                     className='flex items-center gap-1 transition-all duration-300 hover:text-[#FF8A57]'
                                 >
                                     <FaPhoneSquareAlt size={15} />
-                                    <p>+213 699 514 862</p>
+                                    <p
+                                        dir={
+                                            route.locale === "en" &&
+                                            route.locale === "fr"
+                                                ? "rtl"
+                                                : "ltr"
+                                        }
+                                    >
+                                        +213 699 514 862
+                                    </p>
                                 </Link>
                             </div>
                             {/* image container */}
-                            <div className='pr-8 md:hidden'>
+                            <div className='pr-8 sm:hidden'>
                                 <Link href='/home'>
                                     <Image
                                         src='/images/logo-footer.svg'
@@ -107,37 +117,42 @@ function Footer({ t }) {
                     </div>
                 </div>
                 {/* social media links */}
-                <div className='flex justify-center gap-3'>
-                    <Link
-                        href='https://web.facebook.com/recodedofficial'
-                        target='_blank'
-                    >
-                        <FaFacebook
-                            className='text-white hover:text-[#1877f2] transition-all duration-300'
-                            size={25}
-                        />
-                    </Link>
-                    <Link
-                        href='https://www.linkedin.com/school/re-coded/'
-                        target='_blank'
-                    >
-                        <FaLinkedin
-                            className='text-white hover:text-[#0a66c2] transition-all duration-300'
-                            size={25}
-                        />
-                    </Link>
-                    <Link
-                        href='https://www.instagram.com/recodedofficial/'
-                        target='_blank'
-                    >
-                        <FaInstagram
-                            className='text-white hover:text-[#c32aa3] transition-all duration-300'
-                            size={25}
-                        />
-                    </Link>
+                <div className='flex justify-center md:flex-col md:justify-start items-center gap-3 md:w-1/5'>
+                    <h1 className='hidden md:block text-[#FF8A57] font-bold'>
+                        Social
+                    </h1>
+                    <div className='flex gap-3'>
+                        <Link
+                            href='https://web.facebook.com/recodedofficial'
+                            target='_blank'
+                        >
+                            <FaFacebook
+                                className='text-white hover:text-[#1877f2] transition-all duration-300'
+                                size={25}
+                            />
+                        </Link>
+                        <Link
+                            href='https://www.linkedin.com/school/re-coded/'
+                            target='_blank'
+                        >
+                            <FaLinkedin
+                                className='text-white hover:text-[#0a66c2] transition-all duration-300'
+                                size={25}
+                            />
+                        </Link>
+                        <Link
+                            href='https://www.instagram.com/recodedofficial/'
+                            target='_blank'
+                        >
+                            <FaInstagram
+                                className='text-white hover:text-[#c32aa3] transition-all duration-300'
+                                size={25}
+                            />
+                        </Link>
+                    </div>
                 </div>
             </div>
-            <div className='flex justify-center p-2 bg-[#32314C] text-center text-white'>
+            <div className='flex justify-center p-2 bg-[#32314C] text-center text-sm text-white'>
                 <p>
                     {t(
                         "© 2023 Team 10, Re:Coded - Algeria Bootcamp. All rights reserved."
