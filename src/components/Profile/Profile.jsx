8a3d5b9be3c2eea58162a8b10a20cfb958992d4b
Katia Ghezali <ghezalikatia@hotmail.com>
2023-11-25 17:19:42 +0100

    feat(profile page): necessary dashboard fixes
    Create my profile page #153

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -14,6 +14,30 @@ import { db } from "@/util/firebase";
 import { CartContext } from "@/context/CartContext";
 import { UserListingsContext } from "@/context/UserListingsContext";
 import Image from "next/image";
+import {
+    ShoppingCartIcon,
+    CheckCircleIcon,
+    AcademicCapIcon,
+    UserIcon,
+    TruckIcon,
+    SwitchHorizontalIcon,
+} from "@heroicons/react/solid";
+
+<svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='h-6 w-6'
+    fill='none'
+    viewBox='0 0 24 24'
+    stroke='currentColor'
+    strokeWidth={2}
+>
+    <path d='M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z' />
+    <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0'
+    />
+</svg>;
 
 const ProfileComponent = () => {
     const auth = useAuth();
@@ -119,20 +143,7 @@ const ProfileComponent = () => {
                 <section className='grid md:grid-cols-2 xl:grid-cols-4 gap-6'>
                     <div className='flex items-center p-8 bg-white shadow rounded-lg'>
                         <div className='inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-purple-600 bg-purple-100 rounded-full mr-6'>
-                            <svg
-                                aria-hidden='true'
-                                fill='none'
-                                viewBox='0 0 24 24'
-                                stroke='currentColor'
-                                className='h-6 w-6'
-                            >
-                                <path
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                    strokeWidth={2}
-                                    d='M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z'
-                                />
-                            </svg>
+                            <TruckIcon className='h-6 w-6' />
                         </div>
                         <div>
                             <span className='block text-2xl font-bold'>
@@ -146,20 +157,7 @@ const ProfileComponent = () => {
                     </div>
                     <div className='flex items-center p-8 bg-white shadow rounded-lg'>
                         <div className='inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-green-600 bg-green-100 rounded-full mr-6'>
-                            <svg
-                                aria-hidden='true'
-                                fill='none'
-                                viewBox='0 0 24 24'
-                                stroke='currentColor'
-                                className='h-6 w-6'
-                            >
-                                <path
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                    strokeWidth={2}
-                                    d='M13 7h8m0 0v8m0-8l-8 8-4-4-6 6'
-                                />
-                            </svg>
+                            <CheckCircleIcon className='h-6 w-6' />
                         </div>
                         <div>
                             <span className='block text-2xl font-bold'>
@@ -172,21 +170,8 @@ const ProfileComponent = () => {
                     </div>
 
                     <div className='flex items-center p-8 bg-white shadow rounded-lg'>
-                        <div className='inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-blue-600 bg-blue-100 rounded-full mr-6'>
-                            <svg
-                                aria-hidden='true'
-                                fill='none'
-                                viewBox='0 0 24 24'
-                                stroke='currentColor'
-                                className='h-6 w-6'
-                            >
-                                <path
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                    strokeWidth={2}
-                                    d='M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253'
-                                />
-                            </svg>
+                        <div className='inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-yellow-600 bg-yellow-100 rounded-full mr-6'>
+                            <SwitchHorizontalIcon className='h-6 w-6' />
                         </div>
                         <div>
                             <span className='block text-2xl font-bold'>
@@ -199,20 +184,7 @@ const ProfileComponent = () => {
                     </div>
                     <div className='flex items-center p-8 bg-white shadow rounded-lg'>
                         <div className='inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-blue-600 bg-blue-100 rounded-full mr-6'>
-                            <svg
-                                aria-hidden='true'
-                                fill='none'
-                                viewBox='0 0 24 24'
-                                stroke='currentColor'
-                                className='h-6 w-6'
-                            >
-                                <path
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                    strokeWidth={2}
-                                    d='M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253'
-                                />
-                            </svg>
+                            <ShoppingCartIcon className='h-6 w-6' />
                         </div>
                         <div>
                             <span className='block text-2xl font-bold'>
@@ -236,9 +208,15 @@ const ProfileComponent = () => {
                         </div>
                     </div>
 
-                    <div className='row-span-3 bg-white shadow rounded-lg'>
+                    <div className='flex flex-col row-span-3 bg-white shadow rounded-lg'>
                         <div className='flex items-center justify-between px-6 py-5 font-semibold border-b border-gray-100'>
                             <span>Students you dealt with</span>
+                        </div>
+                    </div>
+
+                    <div className='flex flex-col row-span-3 bg-white shadow rounded-lg'>
+                        <div className='px-6 py-5 font-semibold border-b border-gray-100'>
+                            Students you have dealt with
                         </div>
                         <div
                             className='overflow-y-auto'
