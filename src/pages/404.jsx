d5719b141a78e6f401091a1ccb3d9395e0560656
hocine1212 <hocine20992@gmail.com>
2023-11-25 22:30:56 +0100

    feat(notfound page): add redirecting timer to redirect the user after a certain period

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,20 +1,27 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 // import Layout from "@/layout/Layout";
 
 export default function NotFoundPage() {
+    const [countdown, setCountdown] = useState(5);
     const router = useRouter();
 
     useEffect(() => {
         const timer = setTimeout(() => {
             router.push("/home");
         }, 6000);
+        const countdownInterval = setInterval(() => {
+            setCountdown((prevCountdown) => prevCountdown - 1);
+        }, 1000);
 
-        return () => clearTimeout(timer);
-    }, []);
+        return () => {
+            clearTimeout(timer);
+            clearInterval(countdownInterval);
+        };
+    }, [router]);
 
     return (
         <main class='flex min-h-full justify-center py-10 md:py-4 lg:py-1 items-center bg-white px-6 pb-4 sm:pb-12 lg:px-8'>
@@ -28,10 +35,13 @@ export default function NotFoundPage() {
                         alt='404 error'
                     />
                 </div>
-                <p class='mt-2 text-base leading-7 text-gray-600'>
+                <p class='mt-2 text-base leading-7 font-semibold text-gray-600'>
                     Sorry, we couldn’t find the page you’re looking for.
                 </p>
-                <div class='mt-6 mb-0 md:mb-4 flex items-center justify-center gap-x-6'>
+                <div class='mt-2 mb-0 md:mb-4 flex items-center justify-center flex-wrap gap-x-6'>
+                    <p className='mt-2 m-2 sm:mb-0 text-base leading-7 font-semibold text-gray-600'>
+                        Redirecting to the homepage in {countdown} seconds...
+                    </p>
                     <Link
                         href='/home'
                         class='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
