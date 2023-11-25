2d4e842cf7268a350ba14ccddc25a82a8a0cc9db
Katia Ghezali <ghezalikatia@hotmail.com>
2023-11-26 00:22:54 +0100

    feat(checkout page): add a checkout page
    Checkout page #156

---
+++ src/pages/checkout/index.jsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+import Checkout from "@/components/Checkout/Checkout";
+
+const CheckoutPage = () => {
+    return <Checkout />;
+};
+
+export default CheckoutPage;
