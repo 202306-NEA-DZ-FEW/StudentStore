91b32e8acf1284114b754ee6c8077ab7eb849bcc
benarba mohammed <tewfikhotspur@gmail.com>
2023-11-26 03:04:24 +0100

    feat(faq): add q and a to the faq component

--- src/pages/aboutus/index.jsx
+++ src/pages/aboutus/index.jsx
@@ -25,7 +25,7 @@ function AboutUs() {
         const scrollY = window.scrollY || window.pageYOffset;
         const sectionOffset = document.getElementById(
             "technologies-section"
-        ).offsetTop;
+        )?.offsetTop;
 
         if (scrollY > sectionOffset - window.innerHeight / 2) {
             controls.start({ opacity: 1, y: 0, transition: { duration: 0.5 } });
