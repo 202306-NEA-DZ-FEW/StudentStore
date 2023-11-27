c69b4c2675c242234ae942cf0e3bf7aa707e13f7
Katia Ghezali <ghezalikatia@hotmail.com>
2023-11-27 19:13:34 +0100

    feat(checkout): fix styling
    #181

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -33,150 +33,152 @@ const Checkout = () => {
     };
 
     return (
-        <form className='max-w-md mx-auto p-4 bg-white shadow-md rounded-md'>
-            <h2 className='text-2xl font-semibold mb-4'>Checkout</h2>
+        <div className='p-20 flex flex-col'>
+            <form className='max-w-md mx-auto p-4 bg-white shadow-md rounded-md'>
+                <h2 className='text-2xl font-semibold mb-4'>Checkout</h2>
 
-            {/* Delivery Information */}
-            <div className='mb-4'>
-                <label
-                    htmlFor='fullName'
-                    className='block text-gray-700 font-medium'
+                {/* Delivery Information */}
+                <div className='mb-4'>
+                    <label
+                        htmlFor='fullName'
+                        className='block text-gray-700 font-medium'
+                    >
+                        Full Name
+                    </label>
+                    <input
+                        type='text'
+                        id='fullName'
+                        name='fullName'
+                        value={formData.fullName}
+                        onChange={handleChange}
+                        className='mt-1 p-2 w-full border rounded-md'
+                        required
+                    />
+                </div>
+
+                <div className='mb-4'>
+                    <label
+                        htmlFor='address'
+                        className='block text-gray-700 font-medium'
+                    >
+                        Address
+                    </label>
+                    <input
+                        type='text'
+                        id='address'
+                        name='address'
+                        value={formData.address}
+                        onChange={handleChange}
+                        className='mt-1 p-2 w-full border rounded-md'
+                        required
+                    />
+                </div>
+
+                <div className='grid grid-cols-2 gap-4 mb-4'>
+                    <div>
+                        <label
+                            htmlFor='city'
+                            className='block text-gray-700 font-medium'
+                        >
+                            City
+                        </label>
+                        <input
+                            type='text'
+                            id='city'
+                            name='city'
+                            value={formData.city}
+                            onChange={handleChange}
+                            className='mt-1 p-2 w-full border rounded-md'
+                            required
+                        />
+                    </div>
+
+                    <div>
+                        <label
+                            htmlFor='zipCode'
+                            className='block text-gray-700 font-medium'
+                        >
+                            ZIP Code
+                        </label>
+                        <input
+                            type='text'
+                            id='zipCode'
+                            name='zipCode'
+                            value={formData.zipCode}
+                            onChange={handleChange}
+                            className='mt-1 p-2 w-full border rounded-md'
+                            required
+                        />
+                    </div>
+                </div>
+
+                {/* Payment Information */}
+                <div className='mb-4'>
+                    <label
+                        htmlFor='cardNumber'
+                        className='block text-gray-700 font-medium'
+                    >
+                        Card Number
+                    </label>
+                    <input
+                        type='text'
+                        id='cardNumber'
+                        name='cardNumber'
+                        value={formData.cardNumber}
+                        onChange={handleChange}
+                        className='mt-1 p-2 w-full border rounded-md'
+                        required
+                    />
+                </div>
+
+                <div className='grid grid-cols-2 gap-4 mb-4'>
+                    <div>
+                        <label
+                            htmlFor='expiryDate'
+                            className='block text-gray-700 font-medium'
+                        >
+                            Expiry Date
+                        </label>
+                        <input
+                            type='text'
+                            id='expiryDate'
+                            name='expiryDate'
+                            value={formData.expiryDate}
+                            onChange={handleChange}
+                            className='mt-1 p-2 w-full border rounded-md'
+                            required
+                        />
+                    </div>
+
+                    <div>
+                        <label
+                            htmlFor='cvv'
+                            className='block text-gray-700 font-medium'
+                        >
+                            CVV
+                        </label>
+                        <input
+                            type='text'
+                            id='cvv'
+                            name='cvv'
+                            value={formData.cvv}
+                            onChange={handleChange}
+                            className='mt-1 p-2 w-full border rounded-md'
+                            required
+                        />
+                    </div>
+                </div>
+
+                {/* Submit Button */}
+                <button
+                    type='submit'
+                    className='bg-purple-600 text-white py-2 px-4 rounded-md hover:bg-purple-700 focus:outline-none focus:ring focus:border-purple-300'
+                    onClick={handleSubmit}
                 >
-                    Full Name
-                </label>
-                <input
-                    type='text'
-                    id='fullName'
-                    name='fullName'
-                    value={formData.fullName}
-                    onChange={handleChange}
-                    className='mt-1 p-2 w-full border rounded-md'
-                    required
-                />
-            </div>
-
-            <div className='mb-4'>
-                <label
-                    htmlFor='address'
-                    className='block text-gray-700 font-medium'
-                >
-                    Address
-                </label>
-                <input
-                    type='text'
-                    id='address'
-                    name='address'
-                    value={formData.address}
-                    onChange={handleChange}
-                    className='mt-1 p-2 w-full border rounded-md'
-                    required
-                />
-            </div>
-
-            <div className='grid grid-cols-2 gap-4 mb-4'>
-                <div>
-                    <label
-                        htmlFor='city'
-                        className='block text-gray-700 font-medium'
-                    >
-                        City
-                    </label>
-                    <input
-                        type='text'
-                        id='city'
-                        name='city'
-                        value={formData.city}
-                        onChange={handleChange}
-                        className='mt-1 p-2 w-full border rounded-md'
-                        required
-                    />
-                </div>
-
-                <div>
-                    <label
-                        htmlFor='zipCode'
-                        className='block text-gray-700 font-medium'
-                    >
-                        ZIP Code
-                    </label>
-                    <input
-                        type='text'
-                        id='zipCode'
-                        name='zipCode'
-                        value={formData.zipCode}
-                        onChange={handleChange}
-                        className='mt-1 p-2 w-full border rounded-md'
-                        required
-                    />
-                </div>
-            </div>
-
-            {/* Payment Information */}
-            <div className='mb-4'>
-                <label
-                    htmlFor='cardNumber'
-                    className='block text-gray-700 font-medium'
-                >
-                    Card Number
-                </label>
-                <input
-                    type='text'
-                    id='cardNumber'
-                    name='cardNumber'
-                    value={formData.cardNumber}
-                    onChange={handleChange}
-                    className='mt-1 p-2 w-full border rounded-md'
-                    required
-                />
-            </div>
-
-            <div className='grid grid-cols-2 gap-4 mb-4'>
-                <div>
-                    <label
-                        htmlFor='expiryDate'
-                        className='block text-gray-700 font-medium'
-                    >
-                        Expiry Date
-                    </label>
-                    <input
-                        type='text'
-                        id='expiryDate'
-                        name='expiryDate'
-                        value={formData.expiryDate}
-                        onChange={handleChange}
-                        className='mt-1 p-2 w-full border rounded-md'
-                        required
-                    />
-                </div>
-
-                <div>
-                    <label
-                        htmlFor='cvv'
-                        className='block text-gray-700 font-medium'
-                    >
-                        CVV
-                    </label>
-                    <input
-                        type='text'
-                        id='cvv'
-                        name='cvv'
-                        value={formData.cvv}
-                        onChange={handleChange}
-                        className='mt-1 p-2 w-full border rounded-md'
-                        required
-                    />
-                </div>
-            </div>
-
-            {/* Submit Button */}
-            <button
-                type='submit'
-                className='bg-purple-600 text-white py-2 px-4 rounded-md hover:bg-purple-700 focus:outline-none focus:ring focus:border-purple-300'
-                onClick={handleSubmit}
-            >
-                Place Order
-            </button>
-        </form>
+                    Place Order
+                </button>
+            </form>
+        </div>
     );
 };
 
