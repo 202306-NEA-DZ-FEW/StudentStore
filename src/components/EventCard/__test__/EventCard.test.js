7a56257fba9c17f7e1d51686b35af1ec515f2da1
benarba mohammed <tewfikhotspur@gmail.com>
2023-11-27 22:42:04 +0100

    fix(evetcard): fix style in event card

--- src/components/EventCard/__test__/EventCard.test.js
+++ src/components/EventCard/__test__/EventCard.test.js
@@ -2,6 +2,13 @@ import renderer from "react-test-renderer";
 
 import EventCard from "../EventCard";
 
+jest.mock("next/router", () => ({
+    useRouter: () => ({
+        pathname: "/",
+        locale: "en",
+    }),
+}));
+
 const t = (key) => key;
 it("renders correctly", () => {
     const tree = renderer.create(<EventCard t={t} />).toJSON();
