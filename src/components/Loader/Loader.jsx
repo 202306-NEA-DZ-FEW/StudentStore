613fe929d11d50fc8d0b7fa91d71498a0b0ccb0a
hocine1212 <hocine20992@gmail.com>
2023-11-28 11:59:30 +0100

    feat(loader.jsx): fix the Loader component displaying

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,8 +2,8 @@ import { GridLoader } from "react-spinners";
 
 export default function Loader() {
     return (
-        <div className='min-h-[80vh] flex items-center justify-center'>
-            <GridLoader size={22} color={"#FF8A57"} />
+        <div className='fixed top-0 left-0 h-screen w-screen flex items-center bg-[#e5e7eb] justify-center z-[999] '>
+            <GridLoader size={22} color='#FF8A57' />
         </div>
     );
 }
